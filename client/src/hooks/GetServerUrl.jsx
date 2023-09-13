94b257b5cabcdb3e25cb99a5322cb94b2c122ec0
shiref mohammed <shirefmo012345@gmail.com>
2023-09-13 23:42:47 +0200

    update files

--- client/src/hooks/GetServerUrl.jsx
+++ client/src/hooks/GetServerUrl.jsx
@@ -1,6 +1,7 @@
 const GetServerUrl = () => {
   // return "http://localhost:3001";
-  return "https://chat-mern-app-api.vercel.app";
+  // return "https://chat-mern-app-api.vercel.app";
+  return "https://chat-mern-app-api.onrender.com";
 }
 
 export default GetServerUrl
